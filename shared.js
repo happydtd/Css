e687917adfc81cd831e0dc9472d6b32dd65744c8
happydtd <happydtdau@yahoo.com.au>
2022-06-08 17:04:31 +1000

    sec9

---
+++ shared.js
@@ -0,0 +1,42 @@
+var backdrop = document.querySelector('.backdrop');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector(".modal__action--negative");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+for (var i=0; i< selectPlanButtons.length; i++){
+    selectPlanButtons[i].addEventListener('click', function(){
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block'; 
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+    })
+}
+
+function closeModal(){
+    // backdrop.style.display = "none";
+    // modal.style.display = "none";
+    if(modal)
+        modal.classList.remove('open');
+    backdrop.classList.remove('open');
+}
+
+
+
+backdrop.addEventListener("click", function(){
+    //mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+if (modalNoButton)
+    modalNoButton.addEventListener("click", closeModal);
+
+toggleButton.addEventListener("click", function(){
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = "block";
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+});
+
