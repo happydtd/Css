3cce988b20fc7c6b4b2fcf9ea61d1e6323fe6cf3
happydtd <happydtdau@yahoo.com.au>
2022-06-26 21:49:37 +1000

    sec16

--- shared.js
+++ shared.js
@@ -4,13 +4,19 @@ var modal = document.querySelector('.modal');
 var modalNoButton = document.querySelector(".modal__action--negative");
 var toggleButton = document.querySelector(".toggle-button");
 var mobileNav = document.querySelector(".mobile-nav");
+var ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (var i=0; i< selectPlanButtons.length; i++){
     selectPlanButtons[i].addEventListener('click', function(){
         // modal.style.display = 'block';
         // backdrop.style.display = 'block'; 
-        modal.classList.add('open');
-        backdrop.classList.add('open');
+        modal.style.display='block'
+        backdrop.style.display='block';
+        setTimeout(function(){
+            modal.classList.add('open');
+            backdrop.classList.add('open');
+        },10)
+
     })
 }
 
@@ -19,7 +25,14 @@ function closeModal(){
     // modal.style.display = "none";
     if(modal)
         modal.classList.remove('open');
+
     backdrop.classList.remove('open');
+
+    setTimeout(function(){
+        modal.style.display='none'
+        backdrop.style.display='none';
+    },2000)
+
 }
 
 
@@ -37,6 +50,22 @@ toggleButton.addEventListener("click", function(){
     // mobileNav.style.display = 'block';
     // backdrop.style.display = "block";
     mobileNav.classList.add('open');
-    backdrop.classList.add('open');
+    backdrop.style.display='block';
+    setTimeout(function(){
+        backdrop.classList.add('open');
+    },10)
+    
 });
+
+ctaButton.addEventListener('animationstart', function(event){
+
+})
+
+ctaButton.addEventListener('animationend', function(event){
+
+})
+
+ctaButton.addEventListener('animationiteration', function(event){
+
+})
 
